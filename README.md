f4f37e7c11f0a32c01d656afd839412a67e4d7dc
hendrik <hamerlinckhendrik@gmail.com>
2023-02-12 12:15:56 +0100

    place all the builded files in the dist folder

--- README.md
+++ README.md
@@ -9,6 +9,3 @@ runnable as following
 ```
 node src/minmax/builder.mjs && node index.js<example.input
 ```
-
-## Debugging
-When debugging make sure to delete the .js files, otherwise it will cause issues
--- esbuild.mjs
+++ esbuild.mjs
@@ -2,7 +2,9 @@ import {readdirSync, statSync} from "fs"
 import { fileURLToPath } from 'url'
 import {join, dirname} from "path"
 import * as esbuild from "esbuild";
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url);
+const targetDir = join(dirname(__filename), 'dist/exercises');
+const srcDir = join(dirname(__filename), 'src/exercises');
 
 async function buildDir(dir) {
     const entries = readdirSync(dir);
@@ -15,12 +17,14 @@ async function buildDir(dir) {
                 bundle: true,
                 platform: "node",
                 target: ["node12"],
-                outfile: entryDir.replace('.ts', '.js'),
-                sourcemap: true,
+                outfile: entryDir
+                    .replace('.ts', '.js')
+                    .replace(srcDir, targetDir),
+                sourcemap: false,
             });
         }
 
     }
 }
 
-await buildDir(join(dirname(__filename), 'src/exercises'));
+await buildDir(srcDir);
