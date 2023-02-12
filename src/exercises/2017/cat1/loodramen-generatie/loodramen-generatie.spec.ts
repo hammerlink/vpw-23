686c02cf7842cf4967f2ecc7d66cf52bb85da4ce
hendrik <hamerlinckhendrik@gmail.com>
2023-02-12 12:50:50 +0100

    add competition test

--- src/exercises/2017/cat1/loodramen-generatie/loodramen-generatie.spec.ts
+++ src/exercises/2017/cat1/loodramen-generatie/loodramen-generatie.spec.ts
@@ -3,19 +3,27 @@ import {dirname, join} from 'path';
 import {readInputByTestCase, readOutput} from '../../../../engine/input.engine';
 import assert from 'assert';
 
-describe('loodramen generatie', function() {
-   it('should run the opgave', async function() {
-       const handler = loodramenGeneratieHandler;
-       const exampleInputPath = join(dirname(__filename), 'opgave.invoer');
-       await readInputByTestCase(handler, exampleInputPath);
-   });
+describe('loodramen generatie', function () {
+    const handler = loodramenGeneratieHandler;
 
-   it('should run the example', async function() {
-       const handler = loodramenGeneratieHandler;
-       const exampleInputPath = join(dirname(__filename), 'voorbeeld.invoer');
-       const programOutput = await readInputByTestCase(handler, exampleInputPath);
-       const actualOutput = readOutput(join(dirname(__filename), 'voorbeeld.uitvoer'));
-       assert.equal(programOutput.length, actualOutput.length);
-       for (let i = 0; i < programOutput.length; i++) assert.equal(programOutput[i], actualOutput[i]);
-   });
+    it('should run the opgave', async function () {
+        const exampleInputPath = join(dirname(__filename), 'opgave.invoer');
+        await readInputByTestCase(handler, exampleInputPath);
+    });
+
+    it('should run the example', async function () {
+        const exampleInputPath = join(dirname(__filename), 'voorbeeld.invoer');
+        const programOutput = await readInputByTestCase(handler, exampleInputPath);
+        const actualOutput = readOutput(join(dirname(__filename), 'voorbeeld.uitvoer'));
+        assert.equal(programOutput.length, actualOutput.length);
+        for (let i = 0; i < programOutput.length; i++) assert.equal(programOutput[i], actualOutput[i]);
+    });
+
+    it('should run the competition', async function () {
+        const exampleInputPath = join(dirname(__filename), 'wedstrijd.invoer');
+        const programOutput = await readInputByTestCase(handler, exampleInputPath);
+        const actualOutput = readOutput(join(dirname(__filename), 'wedstrijd.uitvoer'));
+        assert.equal(programOutput.length, actualOutput.length);
+        for (let i = 0; i < programOutput.length; i++) assert.equal(programOutput[i], actualOutput[i]);
+    });
 });
