89be896bd2f481f0677d23c4748b04c7b532caa0
hendrik <hamerlinckhendrik@gmail.com>
2023-02-13 09:37:14 +0100

    2017 cat 2 - applijst

---
+++ src/exercises/2017/cat2/applist/applijst.spec.ts
@@ -0,0 +1,33 @@
+import { dirname, join } from 'path';
+import assert from 'assert';
+import {appLijstHandler} from './applijst';
+import {readInputByTestCase, readOutput} from '../../../../engine/input.engine';
+
+
+describe('PLACEHOLDER', function () {
+    const handler = appLijstHandler;
+
+    it('should run the opgave', async function () {
+        const exampleInputPath = join(dirname(__filename), 'opgave.invoer');
+        const programOutput = await readInputByTestCase(handler, exampleInputPath);
+        const actualOutput = readOutput(join(dirname(__filename), 'opgave.uitvoer'));
+        assert.equal(programOutput.length, actualOutput.length);
+        for (let i = 0; i < programOutput.length; i++) assert.equal(programOutput[i], actualOutput[i]);
+    });
+
+    it('should run the example', async function () {
+        const exampleInputPath = join(dirname(__filename), 'voorbeeld.invoer');
+        const programOutput = await readInputByTestCase(handler, exampleInputPath);
+        const actualOutput = readOutput(join(dirname(__filename), 'voorbeeld.uitvoer'));
+        assert.equal(programOutput.length, actualOutput.length);
+        for (let i = 0; i < programOutput.length; i++) assert.equal(programOutput[i], actualOutput[i]);
+    });
+
+    it('should run the competition', async function () {
+        const exampleInputPath = join(dirname(__filename), 'wedstrijd.invoer');
+        const programOutput = await readInputByTestCase(handler, exampleInputPath);
+        const actualOutput = readOutput(join(dirname(__filename), 'wedstrijd.uitvoer'));
+        assert.equal(programOutput.length, actualOutput.length);
+        for (let i = 0; i < programOutput.length; i++) assert.equal(programOutput[i], actualOutput[i]);
+    });
+});
---
+++ src/exercises/2017/cat2/applist/applijst.ts
@@ -0,0 +1,75 @@
+import {dirname, join} from 'path';
+import {readInputByTestCase, TestCaseHandler} from '../../../../engine/input.engine';
+import {lineToNumbers} from '../../../../engine/line-input.engine';
+import assert from 'assert';
+
+
+function swapPositions(appList: number[], i1: number, i2: number): boolean {
+    if (i1 < 0 || i2 < 0) return false;
+    const v1 = appList[i1];
+    appList[i1] = appList[i2];
+    appList[i2] = v1;
+    return true;
+}
+
+function getRequiredSwipesForAppCall(app: number, appList: number[], appsPerScreen: number): number {
+    let index = appList.indexOf(app);
+    let swipes = 0;
+    while (index >= appsPerScreen) {
+        index -= appsPerScreen;
+        swipes++;
+    }
+    return swipes;
+}
+
+function calculateSwipes(appList: number[], appsPerScreen: number, appCalls: number[]): number {
+    let swipes = 0;
+    appCalls.forEach(appIndex => {
+        const app = appList.find(x => x === appIndex);
+        const currentIndex = appList.indexOf(app);
+
+        swipes += getRequiredSwipesForAppCall(app, appList, appsPerScreen);
+
+        swapPositions(appList, currentIndex, currentIndex - 1); // execute app call
+    })
+
+    return swipes;
+}
+
+const handler = (testNumber: number): TestCaseHandler => {
+    let finished = false;
+    let appsPerScreenCount: number | undefined;
+    let appCount: number | undefined;
+    let callCount: number | undefined;
+    let apps: number[] | undefined;
+    let calls: number[] | undefined;
+    const lineHandler = (line: string, logger: (line: string) => void) => {
+        if (appsPerScreenCount === undefined) {
+            const numbers = lineToNumbers(line);
+            assert(numbers.length === 3, `invalid first line ${line}`);
+            appsPerScreenCount = numbers[0];
+            appCount = numbers[1];
+            callCount = numbers[2];
+            return;
+        }
+        if (apps === undefined) return apps = lineToNumbers(line);
+
+        calls = lineToNumbers(line);
+        const swipeCount = calculateSwipes(apps, appsPerScreenCount, calls);
+        logger(`${testNumber} ${swipeCount}`);
+
+        finished = true;
+    };
+    const isDone = () => finished;
+    return {lineHandler, isDone};
+};
+export const appLijstHandler = handler;
+
+if (require.main) {
+    let fileName = undefined;
+    if (process.argv[2]) {
+        const {join, dirname} = require('path');
+        fileName = join(dirname(__filename), process.argv[2]);
+    }
+    readInputByTestCase(handler, fileName).then(_ => null);
+}
