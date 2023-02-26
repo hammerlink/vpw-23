f527bd3b7e4fcd6e89f9d6724bab2b0f9dd28276
hendrik <hamerlinckhendrik@gmail.com>
2023-02-26 13:58:37 +0100

    fix 2021 cat 4 slechterik

--- src/exercises/2021/cat4/slechteriken/slechterik.ts
+++ src/exercises/2021/cat4/slechteriken/slechterik.ts
@@ -28,52 +28,6 @@ function getCoordinates(target: number): { x: number, y: number } {
     return {x, y};
 }
 
-function setBadsBasedOnGood(map: Field) {
-    MapEngine.iterateMap(map, (speler) => {
-        if (!speler) return;
-        if (speler.value.type !== SpelerType.good) return;
-        if (speler.value.target === undefined) return;
-        const bad = speler.value.target;
-        speler.value.target.value.type = SpelerType.bad;
-        speler.value.target.value.target = undefined;
-        if (!map.bads.includes(bad)) map.bads.push(bad);
-    })
-}
-
-function noTargettedIsGood(map: Field) {
-    MapEngine.iterateMap(map, (speler) => {
-        if (!speler) return;
-        if (speler.value.targetedList.filter(x => x.value.type !== SpelerType.bad).length === 0) {
-            speler.value.type = SpelerType.good;
-            if (!map.goods.includes(speler)) map.goods.push(speler);
-        }
-
-    })
-}
-
-function setAdjacentGoodOfNoTargets(map: Field) {
-    MapEngine.iterateMap(map, (speler) => {
-        if (!speler) return;
-        if (speler?.value?.target === undefined) {
-            const adjacents = MapEngine.getAdjacentPoints(map, speler.x, speler.y);
-            adjacents.forEach(x => {
-                if (!x) return;
-                x.value.type = SpelerType.good;
-                if (!map.goods.includes(x)) map.goods.push(x);
-            })
-        }
-    })
-}
-
-function getUnknownOrBad(map: Field): MapLocation<Speler>[] {
-    const outputList: MapLocation<Speler>[] = [];
-    MapEngine.iterateMap(map, (speler) => {
-        if (!speler) return;
-        const type = speler.value.type;
-        if (type === SpelerType.bad) outputList.push(speler);
-    })
-    return outputList;
-}
 
 function printMap(map: Field) {
     console.log();
@@ -84,6 +38,47 @@ function printMap(map: Field) {
         if (type === SpelerType.unknown) return 'u';
         return 'g';
     });
+}
+
+function setSpelerToBad(map: Field, speler: MapLocation<Speler>) {
+    speler.value.type = SpelerType.bad;
+    if (!map.bads.includes(speler)) map.bads.push(speler);
+    const wronglyAccused = speler.value.target;
+    if (wronglyAccused === undefined) return;
+    const removeIndex = wronglyAccused.value.targetedList.indexOf(speler);
+    wronglyAccused.value.targetedList.splice(removeIndex, 1);
+}
+
+function setSpelerToGood(map: Field, speler: MapLocation<Speler>) {
+    speler.value.type = SpelerType.good;
+    if (!map.goods.includes(speler)) map.goods.push(speler);
+    if (speler.value.target !== undefined) setSpelerToBad(map, speler.value.target);
+}
+
+function getOutput(bad1: MapLocation<Speler>, bad2: MapLocation<Speler>): string {
+    assert(bad1 !== bad2);
+    const id1 = bad1.value.id;
+    const id2 = bad2.value.id;
+    if (id1 < id2) return `${id1},${id2}`;
+    return `${id2},${id1}`;
+}
+
+function print(testNumber: number, logger: (line: string) => void, output: string) {
+    logger(`${testNumber} ${output}`);
+}
+
+function checkIfResult(map: Field, testNumber: number, logger: (line: string) => void): boolean {
+    if (map.bads.length === 2) {
+        print(testNumber, logger, getOutput(map.bads[0], map.bads[1]));
+        return true;
+    }
+    if (map.goods.length === 7) {
+        const bads = map.all.filter(x => !map.goods.includes(x));
+        print(testNumber, logger, getOutput(bads[0], bads[1]));
+        return true;
+    }
+
+    return false;
 }
 
 const handler = (testNumber: number): TestCaseHandler => {
@@ -127,23 +122,52 @@ const handler = (testNumber: number): TestCaseHandler => {
         MapEngine.iterateMap(spelerMap, (speler) => {
             if (!speler) return;
             spelerMap.all.push(speler);
-        })
-        setAdjacentGoodOfNoTargets(spelerMap);
-        // printMap(spelerMap);
-        noTargettedIsGood(spelerMap);
-        // printMap(spelerMap);
-        setBadsBasedOnGood(spelerMap);
-        // printMap(spelerMap);
-        noTargettedIsGood(spelerMap);
-        assert(spelerMap.bads.length === 2 || spelerMap.goods.length === 7);
-        if (spelerMap.bads.length !== 2) {
-           spelerMap.bads = spelerMap.all.filter(x => !spelerMap.goods.includes(x));
+        });
+        // minimum pointers =
+        // get all without a target => auto good
+        const noTargets = spelerMap.all.filter(x => x.value.target === undefined);
+        noTargets.forEach(x => {
+            setSpelerToGood(spelerMap, x);
+            // set all adjacent to good
+            MapEngine.getAdjacentPoints(spelerMap, x.x, x.y)
+                .forEach(a => {
+                    if (a !== null && a.value.type !== SpelerType.good) setSpelerToGood(spelerMap, a);
+                });
+        });
+
+        let hasReplaced = false;
+        do {
+            hasReplaced = false;
+            // verify all targets
+            spelerMap.all
+                .filter(x => x.value.target !== undefined && x.value.type === SpelerType.unknown
+                    && x.value.target?.value.type === SpelerType.good)
+                .forEach((x) => {
+                    hasReplaced = true;
+                    setSpelerToBad(spelerMap, x);
+                });
+            if (!hasReplaced) {
+                // check goods if no bads were found
+                spelerMap.all
+                    .filter(x => x.value.target !== undefined && x.value.type === SpelerType.unknown
+                        && x.value.target?.value.type === SpelerType.bad)
+                    .forEach(x => {
+                        hasReplaced = true;
+                        setSpelerToGood(spelerMap, x);
+                    })
+            }
+            if (!hasReplaced) {
+                spelerMap.all.filter(x => x.value.type === SpelerType.unknown && x.value.targetedList.length === 0)
+                    .forEach(x => {
+                        hasReplaced = true;
+                        setSpelerToGood(spelerMap, x);
+                    })
+            }
+        } while (hasReplaced);
+
+        if (!checkIfResult(spelerMap, testNumber, logger)) {
+            printMap(spelerMap);
         }
-
-        let oLine = `${testNumber} `;
-        spelerMap.bads.forEach((x, i) => oLine += `${i > 0 ? ',' : ''}${x.value.id}`);
-        logger(oLine);
-
         finished = true;
     };
     const isDone = () => finished;
