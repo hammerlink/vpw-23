f3da2c96552808b78166284cf8f5e63630f8880e
hendrik <hamerlinckhendrik@gmail.com>
2023-02-27 10:23:50 +0100

    improve template

--- src/exercises/template.ts
+++ src/exercises/template.ts
@@ -2,17 +2,28 @@ import {dirname, join} from 'path';
 import {readInputByTestCase, TestCaseHandler} from '@engine/input.engine';
 import {LineHandler, VPWTestHandler} from '@engine/test.engine';
 
-export const handler = (testNumber: number): TestCaseHandler => {
+const handler = (testNumber: number): TestCaseHandler => {
     const vpwHandler = new VPWTestHandler();
-    const firstLineHandler = new LineHandler((line: string) => {
 
-    }, 1);
-    vpwHandler.handlers.push(firstLineHandler);
+    let min: number | undefined;
+    let max: number | undefined;
+    const numberHandler = new LineHandler((line: string) => {
+        const value = parseInt(line);
+        if (min === undefined || value < min) min = value;
+        if (max === undefined || value > max) max = value;
+    });
+
+    vpwHandler.handlers.push(new LineHandler((line: string) => {
+        numberHandler.amount = parseInt(line);
+    }, 1));
+    vpwHandler.handlers.push(numberHandler);
+
     vpwHandler.resultHandler = new LineHandler((line: string, logger: (line: string) => void) => {
-        logger(`${testNumber}`);
+        logger(`${testNumber} ${min} ${max}`);
     });
     return vpwHandler;
 };
+export default handler;
 
 if (require.main) {
     let fileName = undefined;
